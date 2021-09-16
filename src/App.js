a769b8758a84698219eb580510f71e7f387707dc
“keshavg2” <keshavgupta9906@gmail.com>
2021-09-17 00:17:20 +0530

    add the new features in this app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
 import Movie from './Component/Movies';
 import React , {useState, useEffect} from 'react';
+import Pagination from '@material-ui/lab/Pagination';
 
-const FEATURED_API =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
 const SEARCH_API =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
@@ -12,10 +11,18 @@ const SEARCH_API =
 function App() {
   const [movies,setMovies]=useState([]);
   const [searchTerm, setSearchTerm]=useState('');
+  const [page,setPage]=useState(1);
+  const [entertainment,setEntertainment]=useState('movie');
+  //const [selectValue,setSelectValue]=useState();
+
+  const FEATURED_API =
+    `https://api.themoviedb.org/3/discover/${entertainment}?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=${page}`;
+  console.log(FEATURED_API);
+
   useEffect(()=>{
     getMovies(FEATURED_API);
 
-  },[])
+  },[FEATURED_API])
 
   const getMovies=(API)=>{
     fetch(API)
@@ -30,6 +37,7 @@ function App() {
     e.preventDefault();
       if(searchTerm){  
         getMovies(SEARCH_API+searchTerm);
+        console.log(SEARCH_API+searchTerm);
         setSearchTerm('');
       }
   };
@@ -38,22 +46,67 @@ function App() {
      setSearchTerm(e.target.value);
   };
 
+  // const handleAction=()=>{
+  //   console.log(movies);
+  // }
+
+  function handlePage(event,value){
+     //console.log(event.target.textContext);
+     setPage(value);
+     console.log(page,value);
+  }
+
+  function handleTv(){
+     setEntertainment('tv');
+  }
+
+  function handleMovie(){
+    setEntertainment('movie');
+  }
+
+  // function handleSort(e){
+  //   console.log(e.target.value);
+  //    setSelectValue(e.target.value);
+  //    console.log(selectValue);
+  //    let movie=movies;
+  //    movie.sort(function(a,b){
+  //    return parseFloat(a.selectValue)-parseFloat(b.selectValue);
+  //  })
+  //  setMovies([...movie]);
+  //  console.log(movies);
+  // }
+
   return (
       <>
       <header>
+        <div className="btn">
+        <button onClick={handleTv}>TV</button>
+        <button onClick={handleMovie}>Movie</button>
+        {/* <select 
+        value={selectValue} 
+        onChange={handleSort} 
+      >
+       <option value="">Sort_By</option>
+        <option value="vote_average">Rating</option>
+        <option value="C">Realease_date</option>
+      </select> */}
+        </div>
         <form onSubmit={handleOnSubmit}>
-        <input className="search" type="text" placeholder="search" value={searchTerm} onChange={handleChange}/>
+          <input className="search" type="text" placeholder="search" value={searchTerm} onChange={handleChange}/>
         </form>
       </header>
       <div className="movie-container">
-      {
-       movies.length>0 && movies.map(movie=>{
+       {
+        movies.length>0 && movies.map(movie=>{
          return(
-        <Movie key={movie.id} {...movie}/>
+         <Movie key={movie.id} {...movie} entertainment={entertainment}/>
          )
       })
-     }
+     } 
      </div>
+        <div className="page">
+           <Pagination count={10} page={page} onChange={handlePage} variant="outlined" shape="rounded" />
+        </div>
     </> 
   );
 }
--- src/Component/Movies.js
+++ src/Component/Movies.js
@@ -11,12 +11,13 @@ const IMG_API = "https://image.tmdb.org/t/p/w1280";
          return "red";
        }
  }
- const Movie= ({title,poster_path, overview, vote_average}) =>{
+ const Movie= ({title,poster_path, overview, vote_average,name,entertainment}) =>{
      return(
             <div className="movie">
              <img src={poster_path?IMG_API + poster_path:"https://images.unsplash.com/photo-1460881680858-30d872d5b530?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"} alt=" "/>
              <div className="movie-info">
-                    <h3>{title}</h3>
+                    <h3>{entertainment==='movie' ? title : name}</h3>
+                    {/* {console.log(entertainment)} */}
                     <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
              </div>
 
